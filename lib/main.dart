5af6c7f87608d721b9a66a737598869eca8aaafe
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-03-17 15:20:56 -0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:worldtimeproj/pages/choose_location.dart';
 import 'package:worldtimeproj/pages/home.dart';
+import 'package:worldtimeproj/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    //home: Home(),
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
 }
