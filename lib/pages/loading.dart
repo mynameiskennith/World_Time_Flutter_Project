08749add0391a24c12bb6eac832b34aa58cfe635
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-03-22 02:06:44 -0800

    .

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -12,17 +12,18 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   void getData() async {
-    Response response =
-        await get(Uri.https('jsonplaceholder.typicode.com', 'todos/1'));
-    //A json string is being printed.
-    //i.e. it looks like a object, but it is not, we cannot access each of the data as in key
-    //value pairs, the entire content is a "JSON STRING" !!!
+    //   Response response =
+    //       await get(Uri.https('jsonplaceholder.typicode.com', 'todos/1'));
 
-    // print(response.body);
-    Map data = jsonDecode(response.body);
-    print(data);
+    //   //A json string is being printed.
+    //   //i.e. it looks like a object, but it is not, we cannot access each of the data as in key
+    //   //value pairs, the entire content is a "JSON STRING" !!!
+
+    //   // print(response.body);
+    //   Map data = jsonDecode(response.body);
+    //   print(data);
+    // }
   }
-
   @override
   void initState() {
     super.initState();
