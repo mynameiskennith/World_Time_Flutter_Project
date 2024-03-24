fe7ea63d0713526118cae960db812d0451ede8c3
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-03-23 21:49:31 -0800

    .

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,7 +11,7 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
     //   Response response =
     //       await get(Uri.https('jsonplaceholder.typicode.com', 'todos/1'));
 
@@ -23,11 +23,28 @@ class _LoadingState extends State<Loading> {
     //   Map data = jsonDecode(response.body);
     //   print(data);
     // }
+
+    //Make request
+    Response response =
+        await get(Uri.http('worldtimeapi.org', 'api/timezone/Asia/Kolkata'));
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print('$datetime \n$offset');
+
+    //create a date time object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
+
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
